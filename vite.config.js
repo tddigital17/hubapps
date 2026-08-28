0cbba70ccd34168825e472d6514d187c89598f72
tddigital17 <tekomdevdigital@gmail.com>
2026-08-28 19:39:30 +0800

    udate2

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-// Lokal PC/Laptop
-/* import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-
-export default defineConfig({
-  plugins: [vue()]
-}) */
-
-// Upload ke Github Pages
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
